c6045605595a31dcb72f378815d7cf2d90d19da0
Prince <prince112592@yahoo.com>
2020-02-22 19:07:16 -0800

    Prepare for web

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,14 +4,13 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAie0QM22jlr4Jj3l6mdPXGa5pvdL0WsBM",
-  authDomain: "runtime-terror-1d144.firebaseapp.com",
-  databaseURL: "https://runtime-terror-1d144.firebaseio.com",
-  projectId: "runtime-terror-1d144",
-  storageBucket: "runtime-terror-1d144.appspot.com",
-  messagingSenderId: "70168581355",
-  appId: "1:70168581355:web:5e5d90b4acaa1fa0515aae",
-  measurementId: "G-CX98XPJEMS"
+  apiKey: "AIzaSyBMD75FP14e3VvxTRllOrc26K9vds7WHBs",
+  authDomain: "runtime-terror-web.firebaseapp.com",
+  databaseURL: "https://runtime-terror-web.firebaseio.com",
+  projectId: "runtime-terror-web",
+  storageBucket: "runtime-terror-web.appspot.com",
+  messagingSenderId: "714361214200",
+  appId: "1:714361214200:web:81690bccecf7b7ce18570d"
 };
 
 // Initialize Firebase
